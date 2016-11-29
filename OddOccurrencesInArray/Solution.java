e0e44b057c8c06ab8731b8db374c793c2f8a195f
Zheng Guo <zheng.guo@fujixerox.com>
2016-11-29 15:26:13 +0800

    Update a problem.

--- OddOccurrencesInArray/Solution.java
+++ OddOccurrencesInArray/Solution.java
@@ -1,24 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public static boolean isInteger(String s) {
-        try {
-            Integer.parseInt(s); 
-        } catch(NumberFormatException e) { 
-            return false; 
-        } catch(NullPointerException e) {
-            return false;
+    public static int solution(int A[]) {
+        // Using ArrayList is much more flexible than using Array
+        ArrayList<Integer> arr = new ArrayList<Integer>(A.length);
+        for (int i = 0; i < A.length; i++) {
+            arr.add(Integer.valueOf(A[i]));
         }
 
-        return true;
-    }
+        int ipt = 0;
+        int i = 0;
 
-    public static int solution(int A[]) {
+        while (ipt != arr.size()-1) {
+            i++;
+            if (A[i] == A[ipt]) {
+                arr.remove(i);
+                arr.remove(ipt);
+                i = ipt;
+            } else {
+                if (i == arr.size() - 1) {
+                    ipt +=1;
+                    i = ipt;
+                }
+            }
+        }
+
+        if (arr.size() == 1) {
+            return arr.get(0);
+        } else {
+            return -1;
+        }
     }
 
     public static void main( String[] args ) {
-        int[] sol;
-        int[] inputArray;
         Scanner scan = new Scanner(System.in);
         
         //Decide the number of elements
@@ -30,18 +44,20 @@ class Solution {
         }
 
         //Create a integer array
-		inputArray = new int[N];
+		int[] inputArray = new int[N];
 		for (int i = 0; i < N; i++) {
 			System.out.println("Enter the " + (i+1) + " integer : ");
 		    inputArray[i] = Integer.parseInt(scan.nextLine());
+
             if (inputArray[i] < 1 || inputArray[i] > 1000000000) {
                 System.out.println("Input out of bound!");
                 return;
             }
         }
-
-        sol = solution(inputArray);
-        System.out.println("Final Solution:" + sol);
+        System.out.println("Input Array is: " + Arrays.toString(inputArray));
+        
+        int sol = solution(inputArray);
+        System.out.println("Final Solution: " + sol);
         return;
     }
 }
